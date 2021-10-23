08af08bcd1162d60f103dccc4e7d75681fb4d0d0
Bachellerie Loic <loicbachellerie@me.com>
2021-10-22 23:21:26 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { supabase } from '../api'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+        async () => checkUser()
+    )
+    checkUser()
+    return () => {
+      authListener?.unsubscribe()
+    };
+  }, [])
+  async function checkUser() {
+    const user = supabase.auth.user()
+    setUser(user)
+  }
+
+  return (
+      <div>
+        <nav className="p-6 border-b border-gray-300 space-x-6">
+          <Link href="/">
+            Home
+          </Link>
+          {
+            user && (
+                <Link href="/create-post">
+                  Create a post
+                </Link>
+            )
+          }
+          <Link href="/profile">
+            Profile
+          </Link>
+        </nav>
+        <div className="py-8 px-16">
+          <Component {...pageProps} />
+        </div>
+      </div>
+  )
 }
 
 export default MyApp
